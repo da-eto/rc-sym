e2c6d7a9b8ba612a0edc5b7a17941694e887c7bb
Ilya Pleshakov <da-eto@ya.ru>
2020-02-27 08:16:56 +0300

    Добавил метод получения заведений после заданного

--- src/Repository/PlaceRepository.php
+++ src/Repository/PlaceRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
- * Репозиторий мест в городе.
+ * Репозиторий заведений в городе.
  *
  * @method Place|null find($id, $lockMode = null, $lockVersion = null)
  * @method Place|null findOneBy(array $criteria, array $orderBy = null)
@@ -19,5 +19,24 @@ class PlaceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Place::class);
+    }
+
+    /**
+     * Возвращает заведения с id больше заданного в порядке возрастания id.
+     *
+     * @param int $previousId id заведения, после которого искать
+     * @param int $limit максимальное количество заведений в результате
+     *
+     * @return Place[] найденные заведения
+     */
+    public function findWithGreaterIdOrderedById(int $previousId, int $limit)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.id > :id')
+            ->setParameter('id', $previousId)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
     }
 }
